e2f74dacc53267197d50c704ecf7a4a626c8366d
replicaCortex <replicaCortex@gmail.com>
2025-10-20 18:45:33 +0300

    init commit

---
+++ shell.nix
@@ -0,0 +1,42 @@
+{
+  pkgs ? import <nixpkgs> { },
+  mode ? "dev",
+}:
+let
+  rust = with pkgs; [
+    cargo
+    rustc
+
+    rustfmt
+    clippy
+
+    gdb
+  ];
+
+  multiplex = with pkgs; [
+    rust-analyzer
+    ra-multiplex
+    wayland
+    pkg-config
+  ] ++ rust;
+
+  server_start = ''
+    alias kr="pgrep 'ra-multiplex' | xargs kill"
+    alias sr="ra-multiplex server & nvim --headless src/*.rs"
+    sr
+  '';
+
+  aliases = ''
+    alias cr="cargo run"
+    alias cb="cargo build"
+    alias ct="cargo test"
+    alias cc="cargo-clippy"
+    alias g="rust-gdb -tui ./target/debug/browser_shedule"
+    alias mv="mv -n"
+  '';
+in
+pkgs.mkShell {
+  buildInputs = if mode == "server" then multiplex else rust;
+
+  shellHook = if mode == "server" then server_start else aliases;
+}
